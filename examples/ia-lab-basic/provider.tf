876a9c648313e2037e23d37fd7e8fa20972ccd65
Marcelo Arguello <marcelo.arguello@gmail.com>
2026-07-27 12:06:06 -0300

    Applied TFLint recommendations

--- examples/ia-lab-basic/provider.tf
+++ examples/ia-lab-basic/provider.tf
@@ -6,13 +6,3 @@ provider "aws" {
   }
 }
 
-terraform {
-  required_version = ">= 1.15"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.56"
-    }
-  }
-}
